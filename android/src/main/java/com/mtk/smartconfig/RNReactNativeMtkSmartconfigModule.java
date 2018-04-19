1f7ed6731443f15b7cb8608cf1f95ffe8a838d61
Li Luo <lluo@thoughtworks.com>
2018-04-19 21:54:04 +0800

    [android] - add smartconfig jni lib

---
+++ android/src/main/java/com/mtk/smartconfig/RNReactNativeMtkSmartconfigModule.java
@@ -0,0 +1,62 @@
+
+package com.mtk.smartconfig;
+
+import android.util.Log;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class RNReactNativeMtkSmartconfigModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    public RNReactNativeMtkSmartconfigModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "RNMtkSmartconfig";
+    }
+
+    @ReactMethod
+    public void startConnection(String key, String target, Integer version, Float oldInterval, Float newInterval,
+                                Promise promise) {
+        try {
+            SmartConfigHelper.init();
+            int libVersion = SmartConfigHelper.GetLibVersion();
+            int protoVersion = SmartConfigHelper.GetProtoVersion();
+            Log.i("Smartconfig", "libVersion: " + libVersion + ", protoVersion: " + protoVersion);
+            SmartConfigHelper.initConnection(key, target, version);
+            SmartConfigHelper.setSendInterval(oldInterval, newInterval);
+            promise.resolve(System.currentTimeMillis());
+        } catch (Exception e) {
+            promise.reject(e.getCause());
+        }
+    }
+
+    @ReactMethod
+    public void sendConfiguration(String ssid, String ssidpwd, String authcode,
+                                  Promise promise) {
+        try {
+            SmartConfigHelper.startConnection(ssid, ssidpwd, authcode);
+            promise.resolve(System.currentTimeMillis());
+        } catch (Exception e) {
+            e.printStackTrace();
+            promise.reject(e.getCause());
+        }
+    }
+
+    @ReactMethod
+    public void stopConnection() {
+        try {
+            SmartConfigHelper.stopConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- android/src/main/java/com/mtk/smartconfig/RNReactNativeMtkSmartconfigPackage.java
+++ android/src/main/java/com/mtk/smartconfig/RNReactNativeMtkSmartconfigPackage.java
@@ -10,10 +10,10 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
-public class RNMtkSmartconfigPackage implements ReactPackage {
+public class RNReactNativeMtkSmartconfigPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNMtkSmartconfigModule(reactContext));
+      return Arrays.<NativeModule>asList(new RNReactNativeMtkSmartconfigModule(reactContext));
     }
 
     // Deprecated from RN 0.47
